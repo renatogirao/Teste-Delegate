88b75c57b34b389ba59c9932623d664f9fa38921
Renato Girão <renato.sgirao@gmail.com>
2022-01-27 23:33:57 -0300

    started the protocol delegate creation

---
+++ TesteDelegate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TesteDelegate
+//
+//  Created by Renato Savoia Girão on 27/01/22.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
